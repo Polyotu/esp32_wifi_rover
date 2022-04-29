1419d70821e899bd4bf43e36d7b7cce6b7901ade
Polyotu <55525642+Polyotu@users.noreply.github.com>
2022-04-29 23:56:48 +0900

    コメントを編集

---
+++ rover/rover.ino
@@ -0,0 +1,17 @@
+//ローバー車体制御側のコード
+//udp_esp_rooter_espから引っ張ってきて作る
+
+//CPU:esp32
+//IO:
+//-mpu:BNO055
+//-pwm:pca9685
+//--DCMotorDriver:DRV8835
+//--servoMotor
+//-cam:esp32cam
+//-mp3:DFplayerMini
+//-monitor:SSD1331
+//-gesture:PAJ7620
+//-LED:BlinkM - I2C Controlled RGB LED
+//
+
+//測距と超音波も余裕があれば
